6739fece202eaf7d23aebd7b94543c6170213530
hth_developers <talhamanzoor006@gmail.com>
2018-12-27 19:49:47 +0500

    added pro kit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
+import './assets/css/bootstrap.min.css';
+import './assets/css/paper-kit.css';
+import './assets/css/demo.css';
+
 
 class App extends Component {
   render() {
